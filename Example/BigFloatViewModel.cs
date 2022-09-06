9a77ec9d643aaeca3ecbf4d2beaf2b57814484ba
radj307 <radj307@gmail.com>
2022-09-06 19:37:02 -0400

    fk

---
+++ Example/BigFloatViewModel.cs
@@ -0,0 +1,46 @@
+﻿using radj307;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace Example
+{
+    internal class BigFloatViewModel : INotifyPropertyChanged
+    {
+        public BigFloatViewModel() => _value = new();
+
+        private BigFloat? _value;
+        public BigFloat? Value
+        {
+            get => _value;
+            set
+            {
+                _value = value;
+                NotifyPropertyChanged();
+                NotifyPropertyChanged(nameof(String));
+            }
+        }
+        public string? String
+        {
+            get => Value?.ToString();
+            set
+            {
+                if (value is null)
+                {
+                    Value = null;
+                    NotifyPropertyChanged();
+                    return;
+                }
+                _ = BigFloat.TryParse(value, out _value);
+                NotifyPropertyChanged();
+                NotifyPropertyChanged(nameof(Value));
+            }
+        }
+
+        public string ToString(int precision, bool trailingZeros = false) => Value?.ToString(precision, trailingZeros) ?? string.Empty;
+        public override string ToString() => this.ToString(10, false);
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "") => PropertyChanged?.Invoke(this, new(propertyName));
+    }
+}
---
+++ Example/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Example
+{
+    public static class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            App app = new();
+            try
+            {
+                var mw = new MainWindow();
+                app.Run(mw);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
